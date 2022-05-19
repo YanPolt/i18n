21aa20bd7bafb63cd1c5fd5ba3bf519878040d41
juanpablo barriga <j.barriga@uniandes.edu.co>
2022-05-18 20:32:03 -0500

    Commit deÃl ejercicio ReactI18N

--- src/components/job.js
+++ src/components/job.js
@@ -1,14 +1,33 @@
 import React from "react";
+import { FormattedDate, FormattedNumber } from "react-intl";
 
-const Job = (props) => {
+const Job = ({ offer, unidad }) => {
+  if (offer.salary === 1 && unidad === "millones") {
+    unidad = "millón";
+  }
   return (
     <tr>
-      <th scope="row">{props.offer.id}</th>
-      <td>{props.offer.name}</td>
-      <td>{props.offer.company}</td>
-      <td>{props.offer.salary}</td>
-      <td>{props.offer.city}</td>
-      <td>{props.offer.date}</td>
+      <th scope="row">{offer.id}</th>
+      <td>{offer.name}</td>
+      <td>{offer.company}</td>
+      <td>{offer.salary + " " + unidad}</td>
+      <td>{offer.city}</td>
+      <td>
+        <FormattedDate
+          value={new Date(offer.date)}
+          year="numeric"
+          month="long"
+          day="numeric"
+          weekday="long"
+        />
+      </td>
+      <td>
+        <FormattedNumber
+          value={offer.views}
+          format-style="decimal"
+          minimum-integer-digits="4"
+        />
+      </td>
     </tr>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from "react-intl";
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 import JobsList from "./components/jobslist";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+const navLanguage = navigator.language.split("-")[0];
+const localeMsg = navLanguage === "es" ? localeEsMessages : localeEnMessages;
+
+ReactDOM.render(
+  <IntlProvider locale={navLanguage} messages={localeMsg}>
+    <JobsList language={navLanguage} />
+  </IntlProvider>,
+  document.getElementById("root")
+);
